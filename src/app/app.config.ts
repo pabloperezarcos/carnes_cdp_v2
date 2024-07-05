3887d41a3b9898f0773d68a124edc5a4eabb3f3a
pabloperezarcos <pabloperezarcos@gmail.com>
2024-07-05 12:00:53 -0400

    Update app.config.ts
    Comentarios con COMPODOC añadidos.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,41 @@
-import { ApplicationConfig, importProvidersFrom, LOCALE_ID, NgZone, PLATFORM_ID, APP_INITIALIZER } from '@angular/core';
+/**
+ * Configuración de la aplicación Angular.
+ */
+import { ApplicationConfig, importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { registerLocaleData } from '@angular/common';
-//import { isPlatformBrowser } from '@angular/common';
 import localeEsCL from '@angular/common/locales/es-CL';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
+// Registrar el locale para español de Chile
 registerLocaleData(localeEsCL);
 
+/**
+ * Configuración de la aplicación Angular, incluyendo proveedores de servicios.
+ */
 export const appConfig: ApplicationConfig = {
   providers: [
+    // Proveedor para configurar las rutas de la aplicación
     provideRouter(routes),
+
+    // Proveedor para configurar el cliente HTTP con Fetch y interceptores
     provideHttpClient(withFetch(), withInterceptorsFromDi()),
+
+    // Importar proveedores desde FormsModule para su uso en la aplicación
     importProvidersFrom(FormsModule),
+
+    // Proveedor para configurar el LOCALE_ID con 'es-CL' (español de Chile)
     { provide: LOCALE_ID, useValue: 'es-CL' },
+
+    // Proveedor para inicializar la aplicación Firebase
     provideFirebaseApp(() => initializeApp(environment.firebase)),
+
+    // Proveedor para obtener y proporcionar Firestore de Firebase
     provideFirestore(() => getFirestore()),
   ]
 };
