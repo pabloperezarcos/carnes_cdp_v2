7cef99dcb2f97d288d338cd4d5b6fcacc3f246e7
pabloperezarcos <pabloperezarcos@gmail.com>
2024-07-05 11:54:32 -0400

    Update product.model.ts
    Comentarios con COMPODOC añadidos.

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -8,6 +8,17 @@ export class Product {
     slug: string;
     sku: string;
 
+    /**
+     * Constructor de la clase Product.
+     * @param id Identificador único del producto (opcional).
+     * @param nombre Nombre del producto.
+     * @param descripcionCorta Descripción breve del producto.
+     * @param descripcion Descripción detallada del producto.
+     * @param precio Precio del producto.
+     * @param imagen URL de la imagen asociada al producto.
+     * @param slug Slug del producto para SEO y rutas amigables.
+     * @param sku Código SKU del producto.
+     */
     constructor(
         id: string = '',
         nombre: string = '',
